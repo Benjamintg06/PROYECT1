a3c2da54c7d84f8cd8a839af1ac6575ce4fed769
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2021-06-17 18:49:32 -0400

    Entrada de la aplicación creada

---
+++ APP/src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { JobsProvider } from "./contexts/JobsContext";
+import { CategoryProvider } from "./contexts/CategoryContext";
+import PrivateRoute from "./components/PrivateRoute";
+import { Index } from "./pages/Index";
+import { Profile } from "./pages/Profile";
+import { Detail } from "./pages/Detail";
+import { Post } from "./pages/Post";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { ResetPassword } from "./pages/ResetPassword";
+
+function App() {
+    return (
+        <Router>
+            <AuthProvider>
+                <JobsProvider>
+                    <CategoryProvider>
+                        <Switch>
+                            <Route exact path="/" component={Index}></Route>
+                            <Route
+                                exact
+                                path="/jobs/:uid"
+                                component={Detail}
+                            ></Route>
+                            <PrivateRoute path="/upload" component={Post} />
+                            <PrivateRoute path="/profile" component={Profile} />
+                            <Route
+                                exact
+                                path="/auth/login"
+                                component={Login}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/auth/register"
+                                component={Register}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/auth/forgotpassword"
+                                component={ResetPassword}
+                            ></Route>
+                        </Switch>
+                    </CategoryProvider>
+                </JobsProvider>
+            </AuthProvider>
+        </Router>
+    );
+}
+
+export default App;
